484a263bcd6ca73098cf33eea3fef96e2ea4129a
shimashimashi <n4.ag3a2sh1i@gmail.com>
2018-04-15 01:57:42 +0900

    modify xmonad config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,10 +1,34 @@
 import XMonad
-import XMonad.Hooks.DynamicLog
+import XMonad.Layout
+import XMonad.Layout.IM
+import XMonad.Layout.Named
+import XMonad.Layout.Tabbed
+import XMonad.Layout.Spacing           -- this makes smart space around windows
+import XMonad.Util.EZConfig            -- removeKeys, additionalKeys
+import XMonad.Util.Run(spawnPipe)      -- spawnPipe, hPutStrLn
+import XMonad.Layout.Magnifier         -- this makes window bigger
+import XMonad.Layout.ResizableTile     -- Resizable Horizontal border
+import XMonad.Layout.ThreeColumns      -- for many windows
+import Control.Monad (liftM2)          -- myManageHookShift
+import qualified XMonad.StackSet as W  -- myManageHookShift
+import System.IO                       -- for xmobar
+import XMonad.Hooks.DynamicLog         -- for xmobar
+import XMonad.Hooks.ManageDocks        -- avoid xmobar area
+import XMonad.Layout.DragPane          -- see only two window
+import XMonad.Layout.ToggleLayouts     -- Full window at any time
+import XMonad.Layout.NoBorders         -- In Full mode, border is no use
 
 myModMask = mod4Mask
+
+myLayout =   (spacing 4 $ ResizableTall 1 (3/100) (1/2) [])
+    |||  (magnifiercz 1.2 (ThreeCol 1 (3/100) (1/2)))
+    |||  (spacing 2 $ 
+             (dragPane Horizontal (1/10) (1/2)) ||| (dragPane Vertical   (1/10) (1/2))
+         )
 
 main = xmonad =<< xmobar myConfig
 
 myConfig = defaultConfig {
     modMask = myModMask
+    , layoutHook = toggleLayouts (noBorders Full) $ avoidStruts $ myLayout
 }
